bc5afd21fced0515fbb32df9cba934ce0af61e49
Scott Dixon <scott.dixon@shopify.com>
2018-12-06 12:42:47 +1000

    sub sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {AppProvider, Page, Card, Button, Layout, TextStyle, TextField } from '@shopify/polaris';
+import { AppProvider, Page, Card, Button, Layout, TextStyle, TextField, Heading } from '@shopify/polaris';
 
 class App extends Component {
   state = {
     value: '',
-    json: {}
+    json: [
+      {
+        "name": "theme_info",
+        "theme_name": "Debut",
+        "theme_author": "Shopify",
+        "theme_version": "10.1.1",
+        "theme_documentation_url": "https:\/\/help.shopify.com\/manual\/using-themes\/themes-by-shopify\/debut",
+        "theme_support_url": "https:\/\/support.shopify.com\/"
+      },
+      {
+        "name": "Colors",
+        "settings": [
+          {
+            "type": "color",
+            "id": "color_text",
+            "label": "Titles and headings",
+            "default": "#3d4246"
+          }
+        ]
+      },
+      {
+        "name": "Typography"
+      }
+    ]
   };
+  
+  componentDidMount = () => {
+    this.setState({
+      value: JSON.stringify(this.state.json, null, 4)
+    })
+  }
 
   handleChange = (value) => {
     this.setState({ value });
@@ -23,24 +52,27 @@ class App extends Component {
   render() {
     return (
     <AppProvider>
-      <Page fullWidth title="Example app">
+      <Page fullWidth>
         <Layout>
           <Layout.Section secondary>
           <Card>
-          { this.state.json.map && this.state.json.map((section) => {
-            if (section.name == 'theme_info') {
-              return
-            }
-            return (
-              <Card.Section>
-                <p>{ section.name }</p>
-              </Card.Section>
-            )
-          }) }
-
-            <Card.Section>
-              <p>Typography</p>
-            </Card.Section>
+            { this.state.json.map && this.state.json.map((section) => {
+              if (section.name == 'theme_info') {
+                return
+              }
+              return (
+                <div>
+                  <Card.Section>
+                    <p>{ section.name }</p>
+                  </Card.Section>
+                  <Card sectioned subdued>
+                    <Card sectioned>
+                      <Heading>Online store dashboard</Heading>
+                    </Card>
+                  </Card>
+                </div>
+              )
+            }) }
           </Card>
           </Layout.Section>
           <Layout.Section secondary>
