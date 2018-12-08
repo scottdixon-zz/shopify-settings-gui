fcf9c44a4f1301d2ada0ea293f99f32383b820a2
Scott Dixon <scott.dixon@shopify.com>
2018-12-08 20:59:11 +1000

    Move setting section into its own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import { AppProvider, Page, Card, Layout, TextField, FormLayout, Stack, Badge } from '@shopify/polaris';
+import { AppProvider, Page, Card, Layout, TextField, Stack, Badge } from '@shopify/polaris';
 import Sticky from 'react-stickynode';
-import Input from './components/Input';
 import Toolbar from './components/Toolbar';
-
-// import settingsSchema from './settings_schema.js';
-import { splitByHeaders, translate, inputs } from './utils';
+import SettingSection from './components/SettingSection';
+import { translate, inputs } from './utils';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 class App extends Component {
@@ -104,7 +102,6 @@ class App extends Component {
       window.removeEventListener("resize", this.updateDimensions);
   }
 
-
   outputSchema = () => {
     const output = [...this.state.settingsSchema];
     this.setState({ tempJson: JSON.stringify(output, null, 4) });
@@ -150,49 +147,7 @@ class App extends Component {
               // Some theme settings contain theme_info, we can ignore this
               if (section.name == 'theme_info') return
 
-              return (
-                <div key={translate(section.name)}>
-                  <Card.Section>
-                    <p>{ translate(section.name) }</p>
-                  </Card.Section>
-                  <Card sectioned subdued>
-                    <Droppable droppableId={`${translate(section.name)}`}>
-                      {(provided, snapshot) => (
-                        <div ref={provided.innerRef} {...provided.droppableProps} className={snapshot.isDraggingOver ? 'card-dragging-over preview' : 'preview'}>
-
-                          {
-                            // If there are no settings yet display a message
-                            !section.settings.length && <p className="drop-message">Drop settings here!</p>
-                          }
-
-                          { section.settings && splitByHeaders(section.settings).map(headers => {
-                              // Handle empty sections
-                              if (!headers[0]) return
-
-                              const id = section.name + (headers[0].id || translate(headers[0].content) || translate(headers[0].label))
-                              return (
-                                <Card sectioned key={id} subdued={snapshot.isDraggingOver}>
-                                  <FormLayout>
-                                    { headers && headers.map(setting => {
-                                        // Each input needs a unique ID for drag & drop
-                                        setting.id = setting.id || id + translate(setting.content);
-                                        return (
-                                          <Input {...setting} key={setting.id} />
-                                        )
-                                      })
-                                    }
-                                    {provided.placeholder}
-                                  </FormLayout>
-                                </Card>
-                              )
-                            })
-                          }
-                        </div>
-                      )}
-                    </Droppable>
-                  </Card>
-                </div>
-              )
+              return <SettingSection section={section} key={section.name} />
             })
           }
           </Card>
---
+++ src/components/SettingSection.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { inputs, prettify, translate, splitByHeaders } from '../utils';
+import { Card, Stack, FormLayout } from '@shopify/polaris';
+import Input from './Input';
+
+const SettingSection = ({ section }) => (
+
+  <div key={translate(section.name)}>
+    <Card.Section>
+      <p>{ translate(section.name) }</p>
+    </Card.Section>
+    <Card sectioned subdued>
+      <Droppable droppableId={`${translate(section.name)}`}>
+        {(provided, snapshot) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} className={snapshot.isDraggingOver ? 'card-dragging-over preview' : 'preview'}>
+
+            {
+              // If there are no settings yet display a message
+              !section.settings.length && <p className="drop-message">Drop settings here!</p>
+            }
+
+            { section.settings && splitByHeaders(section.settings).map(headers => {
+                // Handle empty sections
+                if (!headers[0]) return
+
+                const id = section.name + (headers[0].id || translate(headers[0].content) || translate(headers[0].label))
+                return (
+                  <Card sectioned key={id} subdued={snapshot.isDraggingOver}>
+                    <FormLayout>
+                      { headers && headers.map(setting => {
+                          // Each input needs a unique ID for drag & drop
+                          setting.id = setting.id || id + translate(setting.content);
+                          return (
+                            <Input {...setting} key={setting.id} />
+                          )
+                        })
+                      }
+                      {provided.placeholder}
+                    </FormLayout>
+                  </Card>
+                )
+              })
+            }
+          </div>
+        )}
+      </Droppable>
+    </Card>
+  </div>
+);
+
+export default SettingSection;
