dbfaf07d51e4706e3c09f625464e66149598b81c
Scott Dixon <scott.dixon@shopify.com>
2018-12-09 18:16:27 +1000

    Deploy latest

---
+++ docs/precache-manifest.5351232ef8593d879eb60f09774f018e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e55a55f9ba8800dca0",
+    "url": "/static/css/main.972f9ffd.chunk.css"
+  },
+  {
+    "revision": "73e55a55f9ba8800dca0",
+    "url": "/static/js/main.73e55a55.chunk.js"
+  },
+  {
+    "revision": "1b6c1e1e50be0aa859fd",
+    "url": "/static/css/1.770e1383.chunk.css"
+  },
+  {
+    "revision": "1b6c1e1e50be0aa859fd",
+    "url": "/static/js/1.1b6c1e1e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f0309c36334e2d4fba669f66b3a8f49",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2818428790a6209bcbad30425ad10ac6.js"
+  "/precache-manifest.5351232ef8593d879eb60f09774f018e.js"
 );
 
 workbox.clientsClaim();
